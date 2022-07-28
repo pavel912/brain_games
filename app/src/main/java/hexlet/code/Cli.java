0e71f98a02594845b009e151331dc9945804b7bf
plobanov <gta4thebest@mail.ru>
2022-07-28 19:39:42 +0300

    assert compilation of a project

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void GreetUser() {
+        System.out.println("Welcome to the Brain Games!");
+
+        System.out.print("May I have your name? ");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello, " + scan.nextLine() + "!");
+    }
+}
