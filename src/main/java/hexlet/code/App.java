76796936e0509f15c76902daa73a4576f8c322b0
pavel912 <gta4thebest@mail.ru>
2022-08-23 20:28:50 +0300

    move StartMenu functionality to App

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,10 +5,18 @@ import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
 import hexlet.code.games.Progression;
 import hexlet.code.games.Prime;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        String command = StartMenu.getCommand();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
+
+        System.out.print("Your choice: ");
+
+        Scanner scan = new Scanner(System.in);
+
+        String command = scan.nextLine();
 
         switch (command) {
             case "1" -> Cli.greetUser();
